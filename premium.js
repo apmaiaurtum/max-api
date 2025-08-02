9672bfb3b2b6cdb4aa6da396800e51dab928a0f2
apmaiaurtum <miban74415@misehub.com>
2025-08-03 00:37:05 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,16 +1,16 @@
 // streams.js
 const streamMap = {
-  "sky": {
-    url: "https://1rinij81ehes2uswech6drot7durixl2r0.rtgjmfvikmgtgrvkpbg.shop/j5ru6icHofROsespidec64w0s7lgo8ot.m3u8",
-    poster: "https://i.ibb.co/5h7wDqnk/IMG-20250709-105025.jpg"
+  "ClUB": {
+    url: "https://2-fss-2.streamhoster.com/pl_138/207480-6531544-1/playlist.m3u8",
+    poster: ""
   },
   "tencric": {
     url: "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/master.m3u8",
     poster: "#"
   },
-  "fox501": {
-    url: "#",
-    poster: "https://i.ibb.co/dNp10Xf/IMG-20250620-145214.jpg"
+  "WWE": {
+    url: "https://2-fss-2.streamhoster.com/pl_118/207480-6531440-1/playlist.m3u8",
+    poster: "https://i.ibb.co/V0sLqgYx/Od-Xar9-N-1280.webp"
   },
   "CBS": {
     url: "https://fl3.moveonjoy.com/CBS_SPORTS_NETWORK/index.m3u8",
